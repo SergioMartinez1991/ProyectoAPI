4677c549364b7ff679aca040c14b793e00b9c63d
DGTIC <dgtic@MacBook-Air-de-DGTIC.local>
2022-11-17 11:52:06 -0600

    Two commit

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const db = 'NutricionPersonal'
-const dbUser = 'Sergio1991'
-const dbPass = '15dpr0445r.'
+// const db = 'NutricionPersonal'
+// const dbUser = 'Sergio1991'
+// const dbPass = '15dpr0445r.'
 
-const uri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.l1pxxjb.mongodb.net/${db}?retryWrites=true&w=majority`
+// const uri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.l1pxxjb.mongodb.net/${db}?retryWrites=true&w=majority`
 
 
 
-mongoose.connect(uri);
+mongoose.connect(process.env.MONGOURI);
 
 app.use('/NutricionPersonal', require('./src/routes'));
 
--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
-# export PORT=4001
-# export SECRET="SECRET"
-# export MONGOURI='mongodb+srv://Sergio1991:15dpr0445r.@cluster0.l1pxxjb.mongodb.net/NutricionPersonal?retryWrites=true&w=majority'
+export PORT=4001
+export SECRET="SECRET"
+export MONGOURI='mongodb+srv://Sergio1991:15dpr0445r.@cluster0.l1pxxjb.mongodb.net/NutricionPersonal?retryWrites=true&w=majority'
 
 
 
